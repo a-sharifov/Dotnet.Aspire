0a800caeaed098e3406cc94ddb0d256e45f54270
Akber <akber.sharifov2004@gmail.com>
2024-02-22 17:13:24 +0400

    Add retry policy in MigrateDbContext extension in Product.App project

--- crs/Services/Catalog/Catalog.Api/Extensions/ApplicationBuilderExtensions.cs
+++ crs/Services/Catalog/Catalog.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -6,10 +6,17 @@ namespace Catalog.Api.Extensions;
 public static class ApplicationBuilderExtensions
 {
     public static void MigrateDbContext<TDbContext>(this IApplicationBuilder app)
-        where TDbContext : DbContext 
+        where TDbContext : DbContext
     {
         using var scope = app.ApplicationServices.CreateScope();
         using var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
+
+        Policy.Handle<Exception>()
+            .WaitAndRetry(
+            retryCount: 3,
+            _ => TimeSpan.FromSeconds(15))
+            .Execute(dbContext.Database.Migrate);
+
         dbContext.Database.Migrate();
     }
 }
